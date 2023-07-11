0809fb85f27e8c4ef51913bb4410675d5ca5e712
jojotier <122423763+jojotier@users.noreply.github.com>
2023-07-11 13:32:06 -0500

    completed validateInput and formSubmission functions, yet can't get the webpage to work at all... is there a communication issue between the scripts?

--- script.js
+++ script.js
@@ -1,37 +1,23 @@
 // Write your JavaScript code here!
 
 window.addEventListener("load", function() {
-
-  require("./scriptHelper.js");
    
   let form = document.querySelector("form");
 
     form.addEventListener("submit", function(event){
+        event.preventDefault();
         let pilotNameInput = document.querySelector("input[name=pilotName]");
         let copilotNameInput = document.querySelector("input[name=copilotName]");
         let fuelLevelInput = document.querySelector("input[name=fuelLevel]");
         let cargoMassInput = document.querySelector("input[name=cargoMass]");
-
-       /* if(pilotNameInput.value === "" || copilotNameInput.value === "" || fuelLevelInput.value === "" || cargoMassInput.value === ""){
-            alert("Please input data into all fields.");
         
-            event.preventDefault();
-           }*/
-           
-           if (validateInput(pilotNameInput.value) === "Empty" || validateInput(copilotNameInput.value) === "Empty" || validateInput(fuelLevelInput.value) === "Empty" || validateInput(cargoMassInput.value) === "Empty"){
-            alert("Please input data into all fields.");
-        
-            event.preventDefault();
-           }
-        
+       let list = document.getElementById("faultyItems");
+       let pilot = pilotNameInput.value;
+       let copilot = copilotNameInput.value;
+       let fuelLevel = fuelLevelInput.value;
+       let cargoLevel = cargoMassInput.value;
+       formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel);
     })
-
- /*   form.addEventListener("submit", function(event){
-        if (validate.validateInput(fuelLevelInput.value) === true){
-            alert("Not a number. Please input a number.");
-        event.preventDefault();}
-        }
-    )*/
 
    let listedPlanets;
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
--- scriptHelper.js
+++ scriptHelper.js
@@ -1,35 +1,65 @@
 // Write your helper functions here!
 require('isomorphic-fetch');
 
-/*function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
-   // Here is the HTML formatting for our mission target div.
-   /*
-                <h2>Mission Destination</h2>
-                <ol>
-                    <li>Name: </li>
-                    <li>Diameter: </li>
-                    <li>Star: ${star}</li>
-                    <li>Distance from Earth: </li>
-                    <li>Number of Moons: </li>
-                </ol>
-                <img src="">
-   
-}*/
+function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
+        <><h2>Mission Destination</h2><ol>
+        <li>Name: </li>
+        <li>Diameter: </li>
+        <li>Star: ${star}</li>
+        <li>Distance from Earth: </li>
+        <li>Number of Moons: </li>
+    </ol><img src="" /></>}
 
 
-function validateInput(testInput) {
+
+function validateInput(testInput){
 if (testInput = ""){
-    return "Empty";
-}
- if (isNaN(testInput) === true){
-return "Not a number";} else if (isNaN(testInput)) {
-    return "Is a number";
+    return 'Empty';
+} else if (!isNaN(Number(testInput))){
+    return 'Is a number';
+} else {
+    return 'Not a number';
 }
     
 }
 
-/*function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-   
+function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
+
+let pilotStatus = document.getElementbyId("pilotStatus");
+let copilotStatus = document.getElementbyId("copilotStatus");
+let fuelStatus = document.getElementbyId("fuelStatus");
+let cargoStatus = document.getElementbyId("cargoStatus");
+let launchStatus = document.getElementbyId("launchStatus");
+
+if (pilot === 'Empty' || copilot === 'Empty' || fuelLevel === 'Empty' || cargoMass === 'Empty'){
+    alert("Please input data into all fields.");
+}
+
+if (validateInput(fuelLevel) === 'Not a number'){
+    alert("Please input a number for Fuel Level.");
+} else if (validateInput(cargoLevel) === 'Not a number'){
+    alert("Please input a number for Cargo Mass.");
+} else if (validateInput(pilot) === 'Is a number' || validateInput(copilot) === 'Is a number'){
+    alert("Please input letters for the pilot and copilot.");
+} else{
+    pilotStatus.innerHTML = `Pilot ${pilot} is ready.`;
+    copilotStatus.innerHTML = `Pilot ${copilot} is ready.`;
+    list.style.visibility = 'hidden';
+}
+
+if (Number(fuelLevel) < 10000){
+    list.style.visibility = 'visible';
+    fuelStatus.innerHTML = 'You have too little fuel to launch!';
+    launchStatus.innerHTML = '<span style="color:#C7254E">Shuttle is not ready to launch.</span>';
+} else if (Number(cargoLevel > 10000)){
+    list.style.visibility = 'visible';
+    cargoStatus.innerHTML = 'You have too much weight in the cargo hold to launch!';
+    launchStatus.innerHTML = '<span style="color:#C7254E">Shuttle is not ready to launch.</span>';
+} else {
+    list.style.visibility = 'visible';
+    launchStatus.innerHTML = '<span style="color:#419F6A">Shuttle ready for launch!</span>';
+}
+
 }
 
 async function myFetch() {
@@ -42,11 +72,11 @@ async function myFetch() {
 }
 
 function pickPlanet(planets) {
-}*/
+}
 
 
-//module.exports.addDestinationInfo = addDestinationInfo;
+module.exports.addDestinationInfo = addDestinationInfo;
 module.exports.validateInput = validateInput;
-/*module.exports.formSubmission = formSubmission;
+module.exports.formSubmission = formSubmission;
 module.exports.pickPlanet = pickPlanet; 
-module.exports.myFetch = myFetch;*/
+module.exports.myFetch = myFetch;
