22bee7cdb6afc2990e365211c74b17e45beef243
jojotier <122423763+jojotier@users.noreply.github.com>
2023-07-20 17:59:39 -0500

    coded the colors on the launch status to be done programmatically. I'm not entirely sure how to update the message when a new answer is given, though.

--- scriptHelper.js
+++ scriptHelper.js
@@ -54,18 +54,24 @@ if (Number(fuelLevel) < 10000 && Number(cargoLevel) > 10000){
     list.style.visibility = 'visible';
     fuelStatus.innerHTML = 'You have too little fuel to launch! Please raise fuel to above 10000 liters.';
     cargoStatus.innerHTML = 'You have too much weight in the cargo hold to launch! Please drop the weight to below 10000 kilograms.';
-    launchStatus.innerHTML = '<span style="color:#C7254E">Shuttle is not ready to launch.</span>';
+    launchStatus.style.color = "#C7254E";
+    launchStatus.innerHTML = '<span>Shuttle is not ready to launch.</span>';
 } else if (Number(fuelLevel) > 10000 && Number(cargoLevel) > 10000){
     list.style.visibility = 'visible';
     cargoStatus.innerHTML = 'You have too much weight in the cargo hold to launch! Please drop weight to below 10000 kilograms.';
-    launchStatus.innerHTML = '<span style="color:#C7254E">Shuttle is not ready to launch.</span>';
+    fuelStatus.innerHTML = 'Fuel level high enough for launch';
+    launchStatus.style.color = "#C7254E";
+    launchStatus.innerHTML = '<span>Shuttle is not ready to launch.</span>';
 } else if (Number(fuelLevel) < 10000 && Number(cargoLevel) < 10000){
     list.style.visibility = 'visible';
     fuelStatus.innerHTML = 'You have too little fuel to launch! Please raise fuel level to above 10000 liters.';
-    launchStatus.innerHTML = '<span style="color:#C7254E">Shuttle is not ready to launch.</span>';
+    cargoStatus.innerHTML = 'Cargo mass low enough for launch';
+    launchStatus.style.color = "#C7254E";
+    launchStatus.innerHTML = '<span>Shuttle is not ready to launch.</span>';
 } else {
     list.style.visibility = 'visible';
-    launchStatus.innerHTML = '<span style="color:#419F6A">Shuttle ready for launch!</span>';
+    launchStatus.style.color = '#419F6A';
+    launchStatus.innerHTML = '<span>Shuttle ready for launch!</span>';
 }
 
 }
